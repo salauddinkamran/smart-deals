82daf152c75feecfe32354e021d7e88222e7dbdc
salauddin kamran <kamransalauddin186@gmail.com>
2025-11-07 23:15:11 +0600

    dotenv file working

--- smart-deals-client/src/components/MyBids/MyBids.jsx
+++ smart-deals-client/src/components/MyBids/MyBids.jsx
@@ -1,10 +1,105 @@
-import React from 'react';
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import MyContainer from "../MyContainer/MyContainer";
+import Swal from "sweetalert2";
 
 const MyBids = () => {
+  const { user } = use(AuthContext);
+  const [bids, setBids] = useState([]);
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:3000/bids?email=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setBids(data);
+        });
+    }
+  }, [user?.email]);
+
+  const handleDeleteBid = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/bids/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your bid has been deleted.",
+                icon: "success",
+              });
+              const remainingBids = bids.filter(bid => bid._id !== _id)
+              setBids(remainingBids)
+            }
+          });
+      }
+    });
+  };
   return (
-    <div>
-      <h1 className='text-6xl text-red-400 text-center'>My Bids</h1>
-    </div>
+    <MyContainer>
+      <div>
+        <h3>My Bids: {bids.length}</h3>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>SL No.</th>
+                <th>Name</th>
+                <th>Buyer Email</th>
+                <th>Buyer Price</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {bids.map((bid, index) => (
+                <tr key={bid._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{bid.buyer_name}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{bid.buyer_email}</td>
+                  <td>{bid.bid_price}</td>
+                  <td>{bid.status}</td>
+                  <th>
+                    <button
+                      onClick={() => handleDeleteBid(bid._id)}
+                      className="btn btn-outline btn-xs"
+                    >
+                      Remove bid
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </MyContainer>
   );
 };
 
