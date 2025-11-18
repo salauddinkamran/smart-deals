b199c8d540187e56d67c2e18969b37f1d2445669
salauddin kamran <kamransalauddin186@gmail.com>
2025-11-18 11:42:01 +0600

    some change bids api server

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -17,50 +17,24 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
+const verifyFireBaseToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({message: "unauthorized access"})
+  }
+  const token = authorization.split(" ")[1]
+  try {
+    const decode = await admin.auth().verifyIdToken(token);
+    console.log("inside token", decode)
+    req.token_email = decode.email;
+    next()
+  } catch (error) {
+    return res.status(401).send({message: "unauthorized access"})
+  }
+}
+
 const logger = (req, res, next) => {
   console.log("logging information");
-  next();
-};
-
-const verifyFireBaseToken = async (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = req.headers.authorization.split(" ")[1];
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-
-  try {
-    const userInfo = await admin.auth().verifyIdToken(token);
-    req.token_email = userInfo.email;
-    console.log("after token validation", userInfo);
-    next();
-  } catch {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  // verify id token
-};
-
-const verifyJWTToken = (req, res, next) => {
-  console.log("In middleware", req.headers);
-  const authorization = req.headers.authorization;
-  console.log(authorization);
-  if (!authorization) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authorization.split(" ")[1];
-  console.log(token);
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "unauthorized access" });
-    }
-    console.log("after decoded", decoded);
-    req.token_email = decoded.email;
-  });
   next();
 };
 
@@ -116,13 +90,6 @@ async function run() {
 
     // PRODUCTS API
     app.get("/products", async (req, res) => {
-      // const projectFields = { title: 1, price_min: 1, price_max: 1,image: -1 };
-      // const cursor = productsCollection
-      //   .find()
-      //   .sort({ price_min: -1 })
-      //   .skip(2)
-      //   .limit(2)
-      //   .project(projectFields);
       console.log(req.query);
       const email = req.query.email;
       const query = {};
@@ -151,8 +118,8 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/products", async (req, res) => {
-      console.log(req.headers)
+    app.post("/products", verifyFireBaseToken , async (req, res) => {
+      console.log(req.headers);
       const newProduct = req.body;
       const result = await productsCollection.insertOne(newProduct);
       res.send(result);
@@ -179,22 +146,7 @@ async function run() {
       res.send(result);
     });
 
-    // app.get("/bids", logger, verifyFireBaseToken, async (req, res) => {
-    //   // console.log("headers", req)
-    //   const email = req.query.email;
-    //   const query = {};
-    //   if (email) {
-    //     if (email !== req.token_email) {
-    //       return res.status(403).send({ message: "forbidden access" });
-    //     }
-    //     query.buyer_email = email;
-    //   }
-    //   const cursor = bidsCollection.find(query);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
-    app.get("/bids", verifyJWTToken, async (req, res) => {
+    app.get("/bids", verifyFireBaseToken, async (req, res) => {
       const email = req.query.email;
       const query = {};
       if (email) {
@@ -219,7 +171,7 @@ async function run() {
         const query = { product: productId };
         const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
         const result = await cursor.toArray();
-        console.log(result)
+        console.log(result);
         res.send(result);
       }
     );
@@ -247,14 +199,6 @@ async function run() {
       const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
-
-    // app.delete("/bids/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   // const query = { _id: id };
-    //   const result = await bidsCollection.deleteOne(query);
-    //   res.send(result);
-    // });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
